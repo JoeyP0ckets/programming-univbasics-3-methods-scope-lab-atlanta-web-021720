bdc4b491843d815258adb7e9850382a05876f0ab
Joseph Patterson <thinkitmakeituseit@gmail.com>
2020-02-10 02:57:25 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,7 +1,8 @@
 def mario
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-  phrase = "It"
-end
+  phrase = "It's-a me, Mario!"
+  phrase
+  end
 
 def toadstool
   puts status
